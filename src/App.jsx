14934137a1a65d9bc5e04a134d562bf77902cbfd
AndreyGOIT <ae.borga@gmail.com>
2024-10-28 10:22:47 +0200

    Change the UI By Handling Events

--- src/App.jsx
+++ src/App.jsx
@@ -27,20 +27,30 @@
 // }
 
 // export default App
-import PropTypes from 'prop-types'
+// 
 
-export default function App() {
-  const handleClick = (event) => {
-    console.log("Hello World!");
-    console.log(event);
-  }
+import { useState } from 'react';
+
+function App() {
+  // State to hold the visibility status of the paragraph
+  const [isParagraphVisible, setIsParagraphVisible] = useState(true);
+
+  // Function to toggle the visibility status of the paragraph
+  const toggleStatus = () => {
+    setIsParagraphVisible(!isParagraphVisible);
+  };
+
   return (
-    <button onClick={handleClick}>
-      Click me
-    </button>
-  )
+    <>
+      <h1>Change UI based on click</h1>
+      {isParagraphVisible && (
+        <p>This paragraph will be shown/hidden on click</p>
+      )}
+      <button onClick={toggleStatus}>
+        {isParagraphVisible ? 'Hide' : 'Show'} Paragraph
+      </button>
+    </>
+  );
 }
 
-App.propTypes = {
-  user: PropTypes.object
-}
+export default App;
