3cb356ec8a9a86c7d60944cbaf5e597e9cc6d8e9
AndreyGOIT <ae.borga@gmail.com>
2024-10-28 09:07:52 +0200

    add the ChildComponent

--- src/components/ParentComponent.jsx
+++ src/components/ParentComponent.jsx
@@ -1,3 +1,7 @@
 export default function ParentComponent() {
     return <ChildComponent name='John'/>
   }
+
+  function ChildComponent(props){
+    return <p>Hello World! my name is {props.name}</p>
+  }
