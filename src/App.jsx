a8b4314633104e8375fb7600ad028549f1b06542
AndreyGOIT <ae.borga@gmail.com>
2024-10-28 08:56:35 +0200

    add arr of users

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 
 
 function App() {
-  return <h1>Hello World</h1>
+  const users = [
+    { id: 1, name: 'Nathan', role: 'Web Developer' },
+    { id: 2, name: 'John', role: 'Web Designer' },
+    { id: 3, name: 'Jane', role: 'Team Leader' },
+  ]
+  return (
+    <>
+    <h1>Hello World</h1>
+    {/* <h2>Learning to code with React</h2>*/}
+    
+    </>
+  )
 }
 
 export default App
