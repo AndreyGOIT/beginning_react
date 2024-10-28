65893a2bc41f51e08d2575f506b35f82e39a89e0
AndreyGOIT <ae.borga@gmail.com>
2024-10-28 09:53:01 +0200

    React Conditional Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,39 @@
-import ParentComponent from './components/ParentComponent.jsx'
+// import ParentComponent from './components/ParentComponent.jsx'
 
-function App() {
-  const users = [
-    { id: 1, name: 'Nathan', role: 'Web Developer' },
-    { id: 2, name: 'John', role: 'Web Designer' },
-    { id: 3, name: 'Jane', role: 'Team Leader' },
-  ]
-  return (
-    <>
-    <h1 className='text-lowercase'>Hello World</h1>
+// function App() {
+//   const users = [
+//     { id: 1, name: 'Nathan', role: 'Web Developer' },
+//     { id: 2, name: 'John', role: 'Web Designer' },
+//     { id: 3, name: 'Jane', role: 'Team Leader' },
+//   ]
+//   return (
+//     <>
+//     <h1 className='text-lowercase'>Hello World</h1>
     
-    <p>The currently active users list:</p>
-      <ul>
-      {
-        users.map(function(user){
-          // returns Nathan, then John, then Jane
-          return (
-            <li key={user.id}> {user.name} as the {user.role} </li>
-          )
-        })
-      }
-      </ul>
-      <ParentComponent />
-    </>
-  )
+//     <p>The currently active users list:</p>
+//       <ul>
+//       {
+//         users.map(function(user){
+//           // returns Nathan, then John, then Jane
+//           return (
+//             <li key={user.id}> {user.name} as the {user.role} </li>
+//           )
+//         })
+//       }
+//       </ul>
+//       <ParentComponent />
+//     </>
+//   )
+// }
+
+// export default App
+function App(props) {
+  const { user } = props
+
+  if (user) {
+    return <button>Logout</button>
+  }
+  return <button>Login</button>
 }
 
 export default App
