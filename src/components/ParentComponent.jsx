4b4f8c37facf0b22cb6731ceeb8a770650074ddf
AndreyGOIT <ae.borga@gmail.com>
2024-10-28 09:31:41 +0200

    Pass State to a Child Component

--- src/components/ParentComponent.jsx
+++ src/components/ParentComponent.jsx
@@ -1,16 +1,9 @@
 import { useState } from "react"
+import ChildComponent from "./ChildComponent.jsx"
 
  function ParentComponent() {
     const [name, setName] = useState('John');
-    return (
-        <>
-        <h1>Hello {name}</h1>
-        <button onClick={()=> setName('Mark')}>Change name</button>
-        </>
-    )
+    return <ChildComponent name={name} setName={setName} />
   }
 
   export default ParentComponent;
-//   function ChildComponent(props){
-//     return <p>Hello World! my name is {props.name}</p>
-//   }
